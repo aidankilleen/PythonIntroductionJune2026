d9a738fbb9594f68b64fa0bce4ec3f7bd848255d
Aidan Killeen <aidan.killeen@gmail.com>
2026-06-04 11:08:30 +0100

    finished userdao

--- userdao.py
+++ userdao.py
@@ -32,24 +32,53 @@ class UserDao():
         user.id = cur.lastrowid
         return user
         
-    def delete_user(self):
-        pass
-    def update_user(self):
-        pass
+    # TODO - make id a list/tuple or a * parameter        
+    def delete_user(self, id):
+        sql = f"DELETE FROM users WHERE id={id}"
+        self.conn.execute(sql)
+        self.conn.commit()
+        
+    def update_user(self, user):
+        sql = f"""
+        UPDATE users
+        SET name = '{user.name}',
+            email = '{user.email}', 
+            active = {1 if user.active else 0}
+        WHERE id = {user.id}
+        """
+        self.conn.execute(sql)
+        self.conn.commit()
+
     def close(self):
         self.conn.close()
 
 if __name__ == "__main__":
     dao = UserDao()
-    user = User(-1, "Yvonne", "yvonne@gmail.com", True)
-    added_user = dao.add_user(user)
+    #user = User(-1, "Yvonne", "yvonne@gmail.com", True)
+    #added_user = dao.add_user(user)
     # added_user should have an id that was assigned by the db
-    print (added_user)
+    #print (added_user)
 
+    
+    #dao.delete_user(33)
+    #dao.delete_user(37)
+    #dao.delete_user(38)
+    # check 34 is missing below
 
     users = dao.get_users()
     for user in users:
         print (user)
+
+    users[-1].name = "CHANGEDx"
+    users[-1].active = not users[-1].active
+
+    dao.update_user(users[-1])
+
+    users = dao.get_users()
+    for user in users:
+        print (user)
+
+
 
     dao.close()
 
